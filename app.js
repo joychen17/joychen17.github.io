237a59902b1b8d13c5e1c71d86e6599982e95cea
Joy Chen <jchen@Joys-MacBook-Air.local>
2024-04-03 11:52:06 -0700

    Remove accessibility barriers in hamburger menu button

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const menu = document.querySelector('#mobile_menu')
 const menuLinks = document.querySelector('.navbar_menu')
+const menuButton = document.getElementById('menu_button')
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+    console.log(menu.classList.contains("is-active"))
+    if (menu.classList.contains('is-active') == true) {
+        menuButton.value = "HIDE";
+        menuButton.style.background = "white";
+        menuButton.style.color = "#111B0C";
+    } else {
+        menuButton.value = "MENU";
+        menuButton.style.background = "#111B0C";
+        menuButton.style.color = "white";
+    }
 })
